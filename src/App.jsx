f9dacb44c74225de95517f99110f3860566597da
Sanduni Basnayake <sandunikbasnayaka@gmail.com>
2026-03-10 15:38:07 +0530

    structure app layout to centered max-width box format

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navigation from './components/Navigation';
-import NewsTicker from './components/NewsTicker';
-import Footer from './components/Footer';
-import HeroGrid from './components/HeroGrid';
-import NewsFeed from './components/NewsFeed';
-import RightSidebar from './components/RightSidebar';
-import CategoryPage from './pages/CategoryPage';
-import ArticlePage from './pages/ArticlePage';
-import AboutPage from './pages/AboutPage';
-import AdvertisingPage from './pages/AdvertisingPage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import NewsTicker from "./components/NewsTicker";
+import Footer from "./components/Footer";
+import HeroGrid from "./components/HeroGrid";
+import NewsFeed from "./components/NewsFeed";
+import RightSidebar from "./components/RightSidebar";
+import CategoryPage from "./pages/CategoryPage";
+import ArticlePage from "./pages/ArticlePage";
+import AboutPage from "./pages/AboutPage";
+import AdvertisingPage from "./pages/AdvertisingPage";
 
 const Home = () => (
   <div className="w-full">
@@ -32,36 +32,108 @@ const Home = () => (
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-ecn-white text-ecn-black flex flex-col">
-        <Navigation />
-        <NewsTicker />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            {/* Dynamic Single Article Route */}
-            <Route path="/article/:id" element={<ArticlePage />} />
+      {/* Outer gray background bounding the whole viewport */}
+      <div className="min-h-screen flex justify-center bg-gray-200 text-ecn-black">
+        {/* Inner white centered container that mimics the Dasatha Lanka layout box */}
+        <div className="w-full max-w-[1280px] bg-white flex flex-col shadow-2xl min-h-screen overflow-hidden">
+          <Navigation />
+          <NewsTicker />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              {/* Dynamic Single Article Route */}
+              <Route path="/article/:id" element={<ArticlePage />} />
 
-            {/* Category Routes */}
-            <Route path="/main-news" element={<CategoryPage categoryName="ප්‍රධාන පුවත්" categorySlug="main-news" />} />
-            <Route path="/latest" element={<CategoryPage categoryName="අලුත් පුවත්" categorySlug="latest" />} />
-            <Route path="/local" element={<CategoryPage categoryName="දේශීය පුවත්" categorySlug="local" />} />
-            <Route path="/gossip" element={<CategoryPage categoryName="ගොසිප්" categorySlug="gossip" />} />
-            <Route path="/politics" element={<CategoryPage categoryName="දේශපාලන" categorySlug="politics" />} />
-            <Route path="/business" element={<CategoryPage categoryName="ව්‍යාපාරික" categorySlug="business" />} />
-            <Route path="/sports" element={<CategoryPage categoryName="ක්‍රීඩා" categorySlug="sports" />} />
-            <Route path="/arts" element={<CategoryPage categoryName="කලාව" categorySlug="arts" />} />
-            <Route path="/world" element={<CategoryPage categoryName="විදෙස්" categorySlug="world" />} />
-            <Route path="/library" element={<CategoryPage categoryName="පුස්තකාලය" categorySlug="library" />} />
+              {/* Category Routes */}
+              <Route
+                path="/main-news"
+                element={
+                  <CategoryPage
+                    categoryName="ප්‍රධාන පුවත්"
+                    categorySlug="main-news"
+                  />
+                }
+              />
+              <Route
+                path="/latest"
+                element={
+                  <CategoryPage
+                    categoryName="අලුත් පුවත්"
+                    categorySlug="latest"
+                  />
+                }
+              />
+              <Route
+                path="/local"
+                element={
+                  <CategoryPage
+                    categoryName="දේශීය පුවත්"
+                    categorySlug="local"
+                  />
+                }
+              />
+              <Route
+                path="/gossip"
+                element={
+                  <CategoryPage categoryName="ගොසිප්" categorySlug="gossip" />
+                }
+              />
+              <Route
+                path="/politics"
+                element={
+                  <CategoryPage
+                    categoryName="දේශපාලන"
+                    categorySlug="politics"
+                  />
+                }
+              />
+              <Route
+                path="/business"
+                element={
+                  <CategoryPage
+                    categoryName="ව්‍යාපාරික"
+                    categorySlug="business"
+                  />
+                }
+              />
+              <Route
+                path="/sports"
+                element={
+                  <CategoryPage categoryName="ක්‍රීඩා" categorySlug="sports" />
+                }
+              />
+              <Route
+                path="/arts"
+                element={
+                  <CategoryPage categoryName="කලාව" categorySlug="arts" />
+                }
+              />
+              <Route
+                path="/world"
+                element={
+                  <CategoryPage categoryName="විදෙස්" categorySlug="world" />
+                }
+              />
+              <Route
+                path="/library"
+                element={
+                  <CategoryPage
+                    categoryName="පුස්තකාලය"
+                    categorySlug="library"
+                  />
+                }
+              />
 
-            {/* Static Content Routes */}
-            <Route path="/advertising" element={<AdvertisingPage />} />
-            <Route path="/about" element={<AboutPage />} />
-          </Routes>
-        </main>
-        <Footer />
+              {/* Static Content Routes */}
+              <Route path="/advertising" element={<AdvertisingPage />} />
+              <Route path="/about" element={<AboutPage />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
